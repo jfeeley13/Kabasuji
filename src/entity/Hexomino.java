3b74ea4e122bd3a70d29ffd718c160123f0cf67e
TylerChaulk <tcchaulk@wpi.edu>
2016-04-18 17:06:07 -0400

    Completed methods for rotating and flipping hexomino. Added comments to all methods inside of tile, hexTile, and Hexomino.

--- src/entity/Hexomino.java
+++ src/entity/Hexomino.java
@@ -15,14 +15,51 @@ public class Hexomino {
 		this.shape = shape;	
 	}
 	
+	/**
+	 * Rotates Hexomino CW or CCW
+	 * 
+	 * @param direction: true = CW or false = CCW
+	 */
+	
 	public void rotate(boolean direction){
-		
+		//CW  Rotation ---newRow = currCol--newCol = currRow*-1
+		//CCW Rotation ---newRow = currCol*-1-----newCol = currRow
+		//Initialize at CCW Rotation
+		int dir = 1;
+		if(direction)//If CW Rotation
+			dir = -1;
+		for (int TileNum = 0; TileNum >= 5; TileNum++){
+				int currRow = shape[TileNum].getLocation()[0];
+				int currCol = shape[TileNum].getLocation()[1];
+				shape[TileNum].changeLocation(dir*(-1)*currCol, dir*currRow);
+			}
 	}
-	
+	/**
+	 * 
+	 * Flips Hexomino horizontally or vertically 
+	 * 
+	 * @param direction true = VFlip or false = HFlip
+	 */
 	public void flip(boolean direction){
+		//HFlip ---newRow = currRow*-1--newCol = currCol
+		//VFlip ---newRow = currRow-----newCol = currCol*-1
+		//Initialize at HFlip
+		int dir = 1;
+		if(direction)//If VFlip
+			dir = -1;
+		for (int TileNum = 0; TileNum >= 5; TileNum++){
+				int currRow = shape[TileNum].getLocation()[0];
+				int currCol = shape[TileNum].getLocation()[1];
+				shape[TileNum].changeLocation(dir*(-1)*currRow, dir*currCol);
+			}
 		
 	}
 	
+	/**
+	 * 
+	 * 
+	 * @return List of (Row, Column) Coordinates of all the tiles that make up the Hexomino
+	 */
 	public int[][] getCoordShape(){
 		
 		int coords[][] = new int[5][2];
