21b1ab1c7fdba6daf242cefbbfa8eb65318c0f9b
Alex <wootsy12@gmail.com>
2016-05-03 20:36:11 -0400

    Rotating + flippin

--- src/gameControllers/FlipController.java
+++ src/gameControllers/FlipController.java
@@ -51,49 +51,6 @@ public class FlipController implements ActionListener{
 		}
 		this.tile.getBoard().refresh();
 		this.tile.getBoard().drawHex(tile, 1, 1, Color.GREEN);
-		/*//this.tile.getBoard().
-		if(num==1)//If VFlip
-			//dir = -1;
-			
-			tile.
-		for (int TileNum = 0; TileNum >= 5; TileNum++){
-				int currRow = shape[TileNum].getCoords()[0];
-				int currCol = shape[TileNum].getCoords()[1];
-				shape[TileNum].changeLocation(dir*(-1)*currRow, dir*currCol);
-			}
-		
-		
-		 * 
-		 * 
-		 * 
-		tile = MListener.datboi;
-		if(num==1){
-		this.tile.getBoard().rotated = (this.tile.getBoard().rotated % 4) + 1;
-		   if(this.tile.getBoard().rotateCheck(this.tile)) {
-			   this.tile.getBoard().refresh();
-			   this.tile.getBoard().drawHex(this.tile, 1, 1, Color.GREEN);
-		   }
-		   else {
-			   this.tile.getBoard().rotated = (this.tile.getBoard().rotated % 4) - 1;
-		   }
-		}
- 
- 
-	    else {
-		   this.tile.getBoard().rotated = (this.tile.getBoard().rotated % 4) - 1;
-		   if(this.tile.getBoard().rotated<=0) this.tile.getBoard().rotated+=4;
-		   if(this.tile.getBoard().rotateCheck(this.tile)) {
-		   
-			   this.tile.getBoard().refresh();
-			   this.tile.getBoard().drawHex(this.tile, 1, 1, Color.GREEN);
-		   }
-		   else {
-			   if(this.tile.getBoard().rotated==4) 
-				   this.tile.getBoard().rotated-=3;
-			   else
-				   this.tile.getBoard().rotated = (this.tile.getBoard().rotated % 4) + 1;
-		   }
-	   }*/
 		
 	}
 }
